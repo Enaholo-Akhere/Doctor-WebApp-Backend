3cefbab29172130b585aa5344832c3042d3a1368
Enaholo Akhere <69322558+Enaholo-Akhere@users.noreply.github.com>
2026-06-06 18:15:30 +0100

    Cors error fixing (#19)
    * dev_prod
    
    * cors issues fixed
    
    * cors issues fixed
    
    * cors issues fixed

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,7 @@ uncaughtException()
 
 // app.options('*', cors(corsOptions)) // Enable CORS pre-flight for all routes
 app.use((req: Request, res: Response, next: NextFunction): void => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Origin", ["http://localhost:5173", "https://care-connect-ena.netlify.app"]);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Credentials", "true");
