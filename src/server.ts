386d7c70d9a8dde21fd2bd33ca36d46d8ac13de6
Enaholo Akhere <enaholoa@gmail.com>
2026-06-06 17:56:32 +0100

    cors issues fixed

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ const app = express()
 const PORT: number = Number(process.env.PORT) || 3000
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://care-connect-ena.netlify.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
