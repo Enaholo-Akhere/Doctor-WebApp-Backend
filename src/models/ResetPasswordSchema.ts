842034bd97a3fc1f6e189c0c9be08bfbef71e402
Enaholo Akhere <69322558+Enaholo-Akhere@users.noreply.github.com>
2026-06-09 22:36:32 +0100

    working on reset password (#33)

--- src/models/ResetPasswordSchema.ts
+++ src/models/ResetPasswordSchema.ts
@@ -1,10 +1,10 @@
-
 import mongoose from 'mongoose';
-interface resPass { token: string };
 
-const ResetPasswordSchema = new mongoose.Schema<{ token: string, userId: mongoose.ObjectId }>({
+interface resPass { token: string, userId: mongoose.ObjectId };
+
+const ResetPasswordSchema = new mongoose.Schema<resPass>({
     token: { type: String },
-    userId: { type: mongoose.Schema.Types.ObjectId, }
+    userId: { type: mongoose.Schema.Types.ObjectId },
 }, { timestamps: true })
 
 export default mongoose.model('PasswordReset', ResetPasswordSchema);
