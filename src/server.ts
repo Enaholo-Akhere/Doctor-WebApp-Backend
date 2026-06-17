9c92a2f8a23458bdcbfaf2c076748acf32e51341
Enaholo Akhere <enaholoa@gmail.com>
2026-06-17 01:03:04 +0100

    FIX currency

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,8 @@ import { errorHandler } from 'Middleware/errorHandler';
 import { stripeWebhook } from 'Controllers/Bookings/StripeBookingController';
 import { flutterwaveWebhook } from 'Controllers/Bookings/flutterwaveBookingController';
 import helmet from 'helmet';
+import { detectPaymentProvider } from '@utils/paymentProvider';
+import { winston_logger } from '@utils/logger';
 
 const app = express();
 app.set('trust proxy', 1);
@@ -38,8 +40,25 @@ app.use(express.json());
 
 app.use('/api/v1', routers);
 
-app.get('/', (req: Request, res: Response) => {
-    res.status(200).json({ message: "Welcome to CareConnect API" })
+app.get('/', async (req: Request, res: Response) => {
+
+    const ip = (req.headers['x-forwarded-for'] as string)?.split(',')[0].trim() ||
+        req.socket.remoteAddress ||
+        req.ip ||
+        "";
+
+    try {
+        const { currency, exchangeRate, countryCode, provider } = await detectPaymentProvider(ip);
+        console.log('ip address', ip)
+        console.log('currency', currency, 'exchRate', exchangeRate, 'countryCode', countryCode, 'provider', provider)
+        res.status(200).json({ message: "Welcome to CareConnect API", ip, currency, exchangeRate, countryCode, provider })
+    }
+    catch (error: any) {
+        winston_logger.error(error.message.error.stack);
+        res.status(500).json({ message: error.message });
+    }
+
+
 })
 
 
